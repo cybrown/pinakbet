5ba66b3c4b0a48946a2c8e4b50d7a9297ec8e02f
KFlash <YOUR EMAIL ADDRESS>
2016-05-28 23:45:17 +0800

    added travis.yml

---
+++ config/ci/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+echo 'travis_fold:start:TEST'
+
+# used by xvfb that is used by Chrome
+export DISPLAY=:99.0
+
+# Used by karma and karma-chrome-launcher
+export CHROME_BIN=/usr/bin/google-chrome
+
+echo 'travis_fold:start:test.run'
+sh -e /etc/init.d/xvfb start
+gulp test:browser
+echo 'travis_fold:end:test.run'
+
+echo 'travis_fold:end:TEST'
